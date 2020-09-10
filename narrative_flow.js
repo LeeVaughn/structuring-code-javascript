5573158892a2b24419a606f50ac6d470b269d526
LeeVaughn <dlvprogramming@gmail.com>
2020-09-10 14:05:04 -0700

    Create initial narrative flow example app

---
+++ narrative_flow.js
@@ -0,0 +1,29 @@
+function veggies() {
+  options = ["green Peppers", "onions", "mushrooms", "banana peppers"];
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function cheese() {
+  options = ["provolone", "parmesan", "mozzarella"];
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function meat() {
+  options = ["pepperoni", "sausage", "ham"];
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function crust() {
+  options = ["thin", "hand tossed", "pan"];
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function createPizza() {
+  return `Your random pizza is a ${crust()} pizza with ${meat()}, ${veggies()}, and ${cheese()}!`
+}
+
+console.log(createPizza())
